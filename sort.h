595d941a5e7f17d29d37fe88652b4ffba2c497fc
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-22 10:55:43 +0100

    :white_check_mark: Completed task 3

--- sort.h
+++ sort.h
@@ -40,6 +40,7 @@ void bubble_sort(int *array, size_t size);
 void swap_nodes(listint_t *left, listint_t *right);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
