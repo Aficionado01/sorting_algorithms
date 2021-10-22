b99fd343b80e0e921da8158958bfa261f71c9341
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-22 07:42:37 +0100

    :white_check_mark: Completed task 2

--- sort.h
+++ sort.h
@@ -35,7 +35,9 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap_items(int *array, size_t l, size_t r);
 void bubble_sort(int *array, size_t size);
+void swap_nodes(listint_t *left, listint_t *right);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
