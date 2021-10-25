8460fdd23a087383d49bd13803a49e66ede7a666
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-25 14:34:31 +0100

    Updated algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,35 +1,18 @@
 #include "sort.h"
 
 /**
- * swap_items - Swaps two items in an array.
- * @array: The array to modify.
- * @l: The index of the left item.
- * @r: The index of the right item.
- */
-void swap_items(int *array, size_t l, size_t r)
-{
-	int tmp;
-
-	if (array != NULL)
-	{
-		tmp = array[l];
-		array[l] = array[r];
-		array[r] = tmp;
-	}
-}
-
-/**
  * shell_sort - Sorts an array using the shell sort algorithm.
  * @array: The array to sort.
  * @size: The length of the array.
  */
 void shell_sort(int *array, size_t size)
 {
-	int i, j, tmp, gap;
+	int i, j, tmp, gap = 0;
 
 	if ((array == NULL) || (size < 2))
 		return;
-	gap = (size / 3) + 1;
+	while (gap <= ((int)size / 3))
+		gap = (gap * 3) + 1;
 	for (; gap > 0; )
 	{
 		for (i = gap; i < (int)size; i++)
