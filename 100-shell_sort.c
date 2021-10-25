657a9a9f2bacf82b8f278f214d501a1227d863e0
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-25 04:13:05 +0100

    Updated algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -26,12 +26,14 @@ void swap_items(int *array, size_t l, size_t r)
 void shell_sort(int *array, size_t size)
 {
 	int max_val, j;
-	size_t i, gap = 4;
+	size_t i, gap = 0;
 
 	if (array == NULL)
 		return;
 	for (i = 0, max_val = array[0]; i < size; i++)
 		max_val = array[i] > max_val ? array[i] : max_val;
+	while (max_val)
+		gap = (gap * 3 + 1), max_val /= 10;
 	for (; gap > 0; gap = (gap - 1) / 3)
 	{
 		for (i = gap; i < size; i++)
